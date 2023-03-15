acd72cc3dd2a60863f9691c49fa9f7c2b3f60f38
Azurmof <downloadthemapps3@gmail.com>
2023-03-15 01:52:40 -0500

    Fix: weights

--- linear_models/liblinear.go
+++ linear_models/liblinear.go
@@ -61,21 +61,38 @@ func NewProblem(X [][]float64, y []float64, bias float64) *Problem {
 
 func Train(prob *Problem, param *Parameter) *Model {
 	libLinearHookPrintFunc() // Sets up logging
+
 	tmpCProb := C.struct_problem{
 		l:    C.int(prob.c_prob.l),
 		n:    C.int(prob.c_prob.n),
 		y:    (*C.double)(unsafe.Pointer(prob.c_prob.y)),
-		x:    prob.c_prob.x,
+		x:    (**C.struct_feature_node)(unsafe.Pointer(prob.c_prob.x)),
 		bias: C.double(prob.c_prob.bias),
 	}
+
+	cWeightLabel := (*C.int)(C.malloc(C.size_t(len(param.c_param.weight_label)) * C.size_t(unsafe.Sizeof(C.int(0)))))
+	defer C.free(unsafe.Pointer(cWeightLabel))
+
+	cWeight := (*C.double)(C.malloc(C.size_t(len(param.c_param.weight)) * C.size_t(unsafe.Sizeof(C.double(0)))))
+	defer C.free(unsafe.Pointer(cWeight))
+
+	for i, val := range param.c_param.weight_label {
+		*(*C.int)(unsafe.Pointer(uintptr(unsafe.Pointer(cWeightLabel)) + uintptr(i)*unsafe.Sizeof(C.int(0)))) = C.int(val)
+	}
+
+	for i, val := range param.c_param.weight {
+		*(*C.double)(unsafe.Pointer(uintptr(unsafe.Pointer(cWeight)) + uintptr(i)*unsafe.Sizeof(C.double(0)))) = C.double(val)
+	}
+
 	tmpCParam := C.struct_parameter{
 		solver_type:  C.int(param.c_param.solver_type),
 		eps:          C.double(param.c_param.eps),
 		C:            C.double(param.c_param.C),
 		nr_weight:    C.int(param.c_param.nr_weight),
-		weight_label: (*C.int)(unsafe.Pointer(param.c_param.weight_label)),
-		weight:       (*C.double)(unsafe.Pointer(param.c_param.weight)),
+		weight_label: cWeightLabel,
+		weight:       cWeight,
 	}
+
 	return &Model{unsafe.Pointer(C.train(&tmpCProb, &tmpCParam))}
 }
 
