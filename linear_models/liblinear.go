f1bfdc88ccbf87aeeb43d99ee41c5c198fc2c28f
Azurmof <downloadthemapps3@gmail.com>
2023-03-15 00:36:33 -0500

    Fix: cgo argument has Go pointer to Go pointer

--- linear_models/liblinear.go
+++ linear_models/liblinear.go
@@ -60,9 +60,23 @@ func NewProblem(X [][]float64, y []float64, bias float64) *Problem {
 
 func Train(prob *Problem, param *Parameter) *Model {
 	libLinearHookPrintFunc() // Sets up logging
-	tmpCProb := &prob.c_prob
-	tmpCParam := &param.c_param
-	return &Model{unsafe.Pointer(C.train(tmpCProb, tmpCParam))}
+	tmpCProb := C.struct_problem{
+		l:           prob.c_prob.l,
+		n:           prob.c_prob.n,
+		y:           prob.c_prob.y,
+		x:           prob.c_prob.x,
+		bias:        prob.c_prob.bias,
+	}
+	tmpCParam := C.struct_parameter{
+		solver_type: param.c_param.solver_type,
+		eps:         param.c_param.eps,
+		C:           param.c_param.C,
+		nr_weight:   param.c_param.nr_weight,
+		weight_label:param.c_param.weight_label,
+		weight:      param.c_param.weight,
+		init_sol:    param.c_param.init_sol,
+	}
+	return &Model{unsafe.Pointer(C.train(&tmpCProb, &tmpCParam))}
 }
 
 func Export(model *Model, filePath string) error {
